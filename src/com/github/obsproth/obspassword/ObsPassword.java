da81e48fb9b76668e8d8667886acdb505b4aa3ee
obsproth <obsproth@users.noreply.github.com>
2016-11-08 22:34:15 +0900

    added JTable

--- src/com/github/obsproth/obspassword/ObsPassword.java
+++ src/com/github/obsproth/obspassword/ObsPassword.java
@@ -1,9 +1,22 @@
 package com.github.obsproth.obspassword;
 
-public class ObsPassword {
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
+
+public class ObsPassword extends JFrame {
+
+	public ObsPassword() {
+		setSize(600, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//
+		setLayout(new BorderLayout());
+		//
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		new ObsPassword();
 	}
 
 }
