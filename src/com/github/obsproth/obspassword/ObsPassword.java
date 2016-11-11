1a27d63f36031b36a15cebf8ccde13c6eb4c18c4
obsproth <obsproth@users.noreply.github.com>
2016-11-11 21:59:44 +0900

    added csv file read/write

--- src/com/github/obsproth/obspassword/ObsPassword.java
+++ src/com/github/obsproth/obspassword/ObsPassword.java
@@ -1,6 +1,14 @@
 package com.github.obsproth.obspassword;
 
 import java.awt.BorderLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,6 +18,8 @@ import javax.swing.table.DefaultTableModel;
 
 public class ObsPassword extends JFrame {
 
+	private static final String DATA_FILE = "data.csv";
+	
 	private static final String[] COLUMN_NAMES = { "Name", "Length", "BaseHash" };
 
 	DefaultTableModel tableModel;
@@ -31,7 +41,44 @@ public class ObsPassword extends JFrame {
 		setVisible(true);
 	}
 
-	public static void main(String[] args) {
+	public void writeFile() {
+		PrintWriter pw;
+		try {
+			pw = new PrintWriter(DATA_FILE);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+		for (int i = 0; i < tableModel.getRowCount(); i++) {
+			pw.print(table.getValueAt(i, 0));
+			for (int j = 1; j < tableModel.getColumnCount(); j++) {
+				pw.print(',');
+				pw.print(table.getValueAt(i, j));
+			}
+			pw.println();
+		}
+		pw.close();
+	}
+
+
+	public static List<String[]> readFile() throws IOException {
+		List<String[]> list = new ArrayList<>();
+		File file = new File(DATA_FILE);
+		if (file.exists() && file.isFile()) {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String str;
+			while ((str = br.readLine()) != null) {
+				if (!str.isEmpty()) {
+					list.add(str.split(","));
+				}
+			}
+			br.close();
+		}
+		return list;
+	}
+
+
+	public static void main(String[] args) throws IOException {
 		new ObsPassword();
 	}
 
