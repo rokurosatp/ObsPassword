2f5e034863f8d533058866844bb18cfbbf0f73d9
RokuroSatp <a-cclemon-mesopota-root36@live.jp>
2017-11-11 12:47:16 +0900

    add class for password generation customization

--- src/com/github/obsproth/obspassword/common/reductor/Base64Reductor.java
+++ src/com/github/obsproth/obspassword/common/reductor/Base64Reductor.java
@@ -1,5 +1,27 @@
 package com.github.obsproth.obspassword.common.reductor;
+//
+// Base64を利用した英数記号混在ハッシュ→パスワード変換
+//
+import java.util.Base64;
+import java.util.Arrays;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+//
+// Base64を利用した英数記号混在ハッシュ→パスワード変換
+//
+public class Base64Reductor implements IReductor {
 
-public class Base64Reductor {
+    char[] resizedPassword(char[] password, int length) {
+        char[] subChars = Arrays.copyOfRange(password, 0, length);
+        Arrays.fill(password, '\n');
+        return subChars;
+    }
 
+    public char[] generate(byte[] hash, int length) {
+        byte[] encodedBytes = Base64.getEncoder().encode(hash);
+        char[] encoded = Charset.forName("UTF-8")
+            .decode(ByteBuffer.wrap(encodedBytes)).array();
+        Arrays.fill(encodedBytes, (byte)0x20);
+        return resizedPassword(encoded, length);
+    }
 }
