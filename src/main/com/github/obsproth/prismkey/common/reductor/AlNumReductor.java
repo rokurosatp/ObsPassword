af55ac2747012fc5da95a048bbfa736d05f8260d
rokurosatp <a-cclemon-mesopota-root36@live.jp>
2017-11-28 11:22:41 +0900

    fix bug of AlNumReductor generate

--- src/main/com/github/obsproth/prismkey/common/reductor/AlNumReductor.java
+++ src/main/com/github/obsproth/prismkey/common/reductor/AlNumReductor.java
@@ -8,13 +8,13 @@ public class AlNumReductor implements IReductor {
             // やってることは非常に簡単でハッシュの1バイト文に対して64(文字種数)で割った余りを求める
             // a-z,A-Z,0-9のマッピングに基づいて得られる文字を文字列中の対応する部分に割り当てる
             // ハッシュ1バイト -> パスワード１文字　対応になっている。
-            codePoint = hash[i] % 62;
+            codePoint = Byte.toUnsignedInt(hash[i]) % 62;
             if (0 <= codePoint && codePoint < 26) {
                 Character.toChars('a' + codePoint, result, i);
             } else if (26 <= codePoint && codePoint < 52) {
-                Character.toChars('A' + codePoint, result, i);
+                Character.toChars('A' + codePoint - 26, result, i);
             } else {
-                Character.toChars('0' + codePoint, result, i);
+                Character.toChars('0' + codePoint - 52, result, i);
             }
         }
         codePoint = 0;
