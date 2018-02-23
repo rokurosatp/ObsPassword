425c78efcbafeb36bfe5a15ee3f2e0e5cb6ad315
obsproth <obsproth@users.noreply.github.com>
2018-02-23 22:37:50 +0900

    Add GeneratorV2

--- src/main/com/github/obsproth/prismkey/common/generator/GeneratorFactory.java
+++ src/main/com/github/obsproth/prismkey/common/generator/GeneratorFactory.java
@@ -4,7 +4,23 @@ import com.github.obsproth.prismkey.ServiceElement;
 
 public class GeneratorFactory {
 
-	public static GeneratorV1 getGenerator(ServiceElement serviceElement){
-		return new GeneratorV1();
+	private static GeneratorV1 generatorV1 = null;
+
+	private static GeneratorV1 getGeneratorV1() {
+		if (generatorV1 == null) {
+			generatorV1 = new GeneratorV1();
+		}
+		return generatorV1;
+	}
+
+	public static AbstractGenerator getGenerator(ServiceElement serviceElement) {
+		switch (serviceElement.getVersion()) {
+		case 1:
+			return getGeneratorV1();
+		case 2:
+			return new GeneratorV2(serviceElement.config);
+		default:
+			throw new UnsupportedOperationException();
+		}
 	}
 }
---
+++ src/main/com/github/obsproth/prismkey/common/generator/GeneratorV2.java
@@ -0,0 +1,25 @@
+package com.github.obsproth.prismkey.common.generator;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.github.obsproth.prismkey.common.hasher.SaltStretchHasher;
+import com.github.obsproth.prismkey.common.reductor.ExBase58Reductor;
+
+public class GeneratorV2 extends AbstractGenerator {
+
+	private static final String SEED_SALT = "PrismKeyV2";
+	private static final int SEEDHASH_LENGTH = 8;
+	private static final int STRETCHING = 123456;
+
+	public GeneratorV2(List<String> config) {
+		hasher = new SaltStretchHasher(SEED_SALT, SEEDHASH_LENGTH, STRETCHING);
+		Iterator<String> iter = config.iterator();
+		boolean allowNumbers = Boolean.parseBoolean(iter.next());
+		boolean allowCaps = Boolean.parseBoolean(iter.next());
+		boolean allowSmalls = Boolean.parseBoolean(iter.next());
+		String symbols = iter.next();
+		reductor = new ExBase58Reductor(allowNumbers, allowCaps, allowSmalls, symbols);
+	}
+
+}
